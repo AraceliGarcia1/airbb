5d78cd4ff56d278c35639ac6afa09000de83ed97
markleplace <20203tn087@utez.edu.mx>
2022-02-17 12:02:00 -0600

    create

---
+++ app/screens/profile/Create.js
@@ -0,0 +1,162 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Input, Button, Icon } from "react-native-elements";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { isEmpty } from "lodash";
+import { NavigationRouteContext } from "@react-navigation/native";
+//import
+
+export default function LoginForm(props) {
+  //desestructurar props
+  const {navigation}=props;
+  //posicion 0 get, 1 set
+
+  const [showPassword, setShowPassword] = useState(true);
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [error, setError] = useState({ email: "", password: "" });
+
+  const change = (event, type) => {
+    setFormData({ ...formData, [type]: event.nativeEvent.text });
+    console.log(formData);
+  };
+
+  const login = () => {
+    if (isEmpty(formData.email) || isEmpty(formData.password) || isEmpty(formData.confirm_password)) {
+      setError({
+        email: "Campo obligatorio",
+        password: "Campo obligatorio",
+        confirm_password:"Campo obligatorio"
+      });
+    } else {
+      //Inicio de sesión
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, formData.email, formData.password,formData.confirm_password)
+        //me regresa una respuesta y por eso ocuapamos el  .then que ejecuta mi promesa
+        .then((userCredential) => {
+          // Signed in
+          //const user = userCredential.user;
+          //console.log(user);
+          // ...
+
+          navigation.navigate("profileStack")
+        })
+        .catch((error) => { //implementar los codigos de errores 
+          console.log("Usuario y contraseñas incorrectas", error);
+        });
+      setError({
+        email: "",
+        password: "",
+      });
+    }
+  /////////////////////////
+  const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+
+
+
+
+
+  };
+
+
+
+  return (
+    <View style={styles.container}>
+      <Input
+        placeholder="20203tn087@utez.edu.mx"
+        keyboardType="email-address"
+        rightIcon={
+          <Icon
+            type="material-community"
+            name="email-outline"
+            size={20}
+            color="#ff5a60"
+          />
+        }
+        label="Correo Electronico"
+        containerStyle={styles.containerInput}
+        labelStyle={styles.labelInput}
+        // onChange={(event)=>console.log(event)}
+        onChange={(event) => change(event, "email")}
+        errorMessage={error.email}
+      />
+      <Input
+        placeholder="***********"
+        rightIcon={
+          <Icon
+            type="material-community"
+            name={showPassword ? "eye-off-outline" : "eye-outline"}
+            size={20}
+            color="#ff5a60"
+            onPress={() => setShowPassword(!showPassword)}
+          />
+        }
+        label="Contraseña:*"
+        containerStyle={styles.containerInput}
+        labelStyle={styles.labelInput}
+        secureTextEntry={showPassword}
+        onChange={(event) => change(event, "password")}
+        errorMessage={error.password}
+      />
+
+      <Button
+        title="Iniciar Sesion"
+        containerStyle={styles.btnContainer}
+        buttonStyle={styles.btn}
+        icon={
+          <Icon name="sign-in" type="font-awesome" color="#fff" size={20} />
+        }
+        iconContainerStyle={{ marginRight: 10 }}
+        onPress={login}
+      />
+      <Text style={styles.textCreateAccount}>
+        <Icon
+          type="material-community"
+          name="account-plus"
+          size={20}
+          color="#1E84B6"
+        />
+        Crear Cuenta
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 16,
+  },
+  containerInput: {
+    width: "100%",
+    marginBottom: 20,
+  },
+  labelInput: {
+    fontSize: 20,
+    color: "#ff5a60",
+  },
+  btnContainer: {
+    width: "70%",
+  },
+  btn: {
+    color: "#fff",
+    backgroundColor: "#ff5a60",
+  },
+  textCreateAccount: {
+    color: "#1E84B6",
+    marginTop: 16,
+  },
+});
