4892327778120fc74b334b58bc7815e3b28724d1
markleplace <20203tn087@utez.edu.mx>
2022-03-09 10:48:52 -0600

    mapa

--- app/components/travel/AddHouseForm.js
+++ app/components/travel/AddHouseForm.js
@@ -5,15 +5,26 @@ import *as ImagePicker from "expo-image-picker"
 import {map, size,filter} from "lodash"
 import * as Location from "expo-location"
 import MapView from "react-native-maps"
+import Modal from '../../utils/Modal'
 import {db} from "../../utils/firebase"
 import {collection,addDoc} from "firebase/firestore"
 
 const widthScreen=Dimensions.get("window").width
 
 export default function AddHouseForm(props) {
+     //generacion de estados con el const
      const {toastRef, setLoading}=props
      const [imageSelected,setImageSelected]=useState([])
      const [error, setError]=useState({camera:"",place:"",address:"",description:""})
+     const [isVisibleMap,setIsVisibleMap]=useState(false)
+     const [place,setPlace]=useState("")
+     const [address,setAddress]=useState("")
+     const [description,setDescription]=useState("")
+     const [locationHouse,setLocationHouse]=useState(null)
+     const saveHouse=()=>{
+          console.log("hola")
+     }
+
     
   return (
       <ScrollView>
@@ -26,9 +37,41 @@ export default function AddHouseForm(props) {
            setImageSelected={setImageSelected}
            imageSelected={imageSelected}
            error={error}
-           
+           />
+           <FormAdd
+           setIsVisibleMap={setIsVisibleMap}
+           setPlace={setPlace}
+           setDescription={setDescription}
+           setAddress={setAddress}
+           error={error}
+           locationHouse={locationHouse}
+           />
+           <Button
+           title="Crear condominio"
+           buttonStyle={styles.btn}
+           icon={
+                <Icon
+                type="font-awesome"
+                name="save"
+                size={15}
+                color="white"
+                />
+
+                
+           }
+           iconContainerStyle={{marginRight:10}}
+           onPress={saveHouse}
            
            />
+           <Map
+           isVisibleMap={isVisibleMap} 
+           setIsVisibleMap={setIsVisibleMap}
+           toastRef={toastRef}
+           setLocationHouse={setLocationHouse}
+           
+           />
+       
+           
       </ScrollView>
   )
 }
@@ -63,7 +106,7 @@ function UploadImage(props){
                     aspect:[3,2]
                })
                if(!result.cancelled){
-                    setImageSelected(...imageSelected,result.uri)
+                    setImageSelected([...imageSelected,result.uri])
 
                }else{
                     toastRef.current.show("Has cerrado galeria")
@@ -125,6 +168,125 @@ function UploadImage(props){
 
 }
 
+function Map(props){
+ const {isVisibleMap,setIsVisibleMap,toastRef,setLocationHouse}=props
+ const[location,setLocation]=useState()
+ useEffect(()=>{
+      (async()=>{
+           const resultPermissions=await Location.requestForegroundPermissionsAsync
+           if(resultPermissions.status==="granted"){
+                let loc=await Location.getCurrentPositionAsync({})
+                setLocation({
+                     latitude:loc.coords.latitude,
+                     longitude:loc.coords.longitude,
+                     latitudDelta:0.001, //estatico
+                     longitudeDelta:0.001 //estatico
+
+                })
+
+           }else{
+                toastRef.current.show("Es necesario aceptar los permisos de ubicacion ")
+           }
+
+      })()
+
+ },[])
+ const confirmLocation=()=>{
+     setLocationHouse(location)
+     toastRef.current.show("Ubicacion guardada")
+     setIsVisibleMap(false)
+ }
+ return(
+      <Modal isVisible={isVisibleMap} setIsVisible={setIsVisibleMap}>
+           <View>
+                //validamos si location tiene un dato
+                {location &&(
+                     <MapView
+                     style={styles.map}
+                     initialRegion={location}
+                     showsUserLocation={true}
+                     //setea la region actual, cada que mueva el mapa 
+                     onRegionChange={(region)=>setLocation}
+                     >
+                          <MapView.Marker
+                          coordinate={{
+                               latitude:location.latitude,
+                               longitude:location.longitude
+                          }}
+                          draggable
+                          />
+
+
+                     </MapView>
+                )}
+                <View style={{flex:1,alignItems:"center", marginTop:10}}>
+                     <Divider style={styles.divider}/>
+                </View>
+                <View>
+                     <Button
+                     title="Cancelar"
+                     containerStyle={styles.btnContainer}
+                     buttonStyle={styles.btnStyleCancel}
+                     onPress={()=>setIsVisibleMap(false)}
+                     
+                     />
+                      <Button
+                     title="Guardar Ubicacion"
+                     containerStyle={styles.btnContainer}
+                     buttonStyle={styles.btnStyleSave}
+                     onPress={confirmLocation}
+                     
+                     />
+                </View>
+           </View>
+      </Modal>
+ )
+}
+
+function FormAdd(props){
+     const{setIsVisibleMap,setPlace,setDescription,setAddress,error,locationHouse}=props
+     return(
+          <View style={styles.viewForm}>
+               <Input
+               label="Lugar"
+               labelStyle={styles.label}
+               placeholder="Acapulco"
+               containerStyle={styles.inputContainer}
+               errorMessage={error.place}   
+               onChange={(event)=>setPlace(event.nativeEvent.text)}         
+               />
+               <Input
+               label="Direccion"
+               labelStyle={styles.label}
+               placeholder="Colonia Laureles"
+               containerStyle={styles.inputContainer}
+               errorMessage={error.address}   
+               onChange={(event)=>setAddress(event.nativeEvent.text)} 
+               rightIcon={
+                    <Icon
+                    type='material-community'
+                    name='google-maps'
+                    color={locationHouse ? "#00a680": "#c2c2c2"}
+                    onPress={()=>setIsVisibleMap(true)}
+                    
+                    />
+               }        
+               />
+               <Input
+               label="Description"
+               labelStyle={styles.label}
+               placeholder="Comentarios"
+               containerStyle={styles.inputContainer}
+               inputContainerStyle={styles.textArea}
+               errorMessage={error.description}   
+               multiline={true}
+               onChange={(event)=>setDescription(event.nativeEvent.text)}         
+               />
+
+
+          </View>
+     )
+}
 
 const styles = StyleSheet.create({
 
@@ -147,6 +309,52 @@ const styles = StyleSheet.create({
           width:70,
           height:70,
           marginRight:10
+     },
+     viewForm:{
+          marginLeft:10,
+          marginRight:10,
+          marginTop:16
+     },
+     label:{
+          fontSize:15,
+          color:"#ff5a60"    
+     },
+     inputContainer:{
+          marginBottom:16,
+     },
+     textArea:{
+          height:100,
+          width:"100%"
+
+     },
+     btn:{
+          backgroundColor:"#ff5a60",
+          margin:20
+     },
+     map:{
+          width:"100%",
+          height:560,
+
+     },
+     divider:{
+          width:"85%",
+          backgroundColor:"#ff5a60",
+          marginBottom:2,
+          
+     },
+     btnContainer:{
+          padding:5,
+          marginBottom:10,
+     },
+     btnStyleCancel:{
+          backgroundColor:"#a60a0d"
+
+     },
+     btnStyleSave:{
+          backgroundColor:"#00a680"
      }
+
+
+
 
 })
